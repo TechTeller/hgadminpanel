fe627a9de6420c6471545d032265683c13aff232
Evan Louis Robinson <evan@danacita.com>
2022-11-28 17:17:10 +0700

    Fix bug from missed file during code cleanup

--- src/pages/channelReminders/index.tsx
+++ src/pages/channelReminders/index.tsx
@@ -1,16 +1,19 @@
 import { NextPage } from "next";
 import AdminList from "@/components/AdminList";
 import Layout from "@/components/Layout";
+import { trpc } from "@/utils/trpc";
 
 const ChannelReminderAdminList: NextPage = () => {
+  const res = trpc.channelReminder.getAll.useQuery();
   return (
     <Layout>
       <AdminList
         columnProps={[
-          { source: "channelName", label: "Channel" },
-          { source: "message_interval", label: "Message Interval" },
-          { source: "embedName", label: "Embed" },
+          { fieldName: "channelName", headerName: "Channel" },
+          { fieldName: "message_interval", headerName: "Message Interval" },
+          { fieldName: "embedName", headerName: "Embed" },
         ]}
+        rowData={res.data ?? []}
         slug="channelReminders"
         title="Channel Reminders"
       />
